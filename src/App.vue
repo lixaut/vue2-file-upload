e36163c79ef218e2d79f5ef0290269cf2cdf7464
Berg Qin <wlkp9108@gmail.com>
2023-02-07 18:23:03 +0800

    【主页布局】开发完成

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,67 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container class="container">
+      <!-- 头部标题 -->
+      <el-header>文件上传的多种方式</el-header>
+      <el-container>
+        <!-- 侧边导航栏 -->
+        <el-aside>
+          <el-menu>
+            <el-menu-item index="1">
+              <i class="el-icon-upload"></i>
+              <span>方式1</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-upload"></i>
+              <span>方式2</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-upload"></i>
+              <span>方式3</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <!-- 主内容区 -->
+        <el-main>main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  .container {
+    height: 100vh;
+    .el-header {
+      font-size: 24px;
+      text-align: center;
+      line-height: 60px;
+      background-color: #eee;
+    }
+    .el-container {
+      .el-aside {
+        border-right: 2px dashed #eee;
+        .el-menu {
+          border-right: none;
+        }
+      }
+      .el-main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入 element-ui
+import { 
+  Container,
+  Header,
+  Aside,
+  Main,
+  MenuItem,
+  Menu
+} from 'element-ui'
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(MenuItem)
+
 Vue.config.productionTip = false
 
 new Vue({
